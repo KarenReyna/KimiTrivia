f884989b7fa2f88035c6d585dcaad08a8179e563
KarenReyna <karen.reyna95@gmail.com>
2016-03-18 20:44:13 -0600

    Base de proyecto

---
+++ KimiTrivia/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KimiTrivia
+//
+//  Created by lizbeth garza walle on 18/03/16.
+//  Copyright © 2016 Ana Karen Reyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
