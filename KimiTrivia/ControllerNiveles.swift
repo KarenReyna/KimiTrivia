14405ba2a50371cfe8e8e00db16bd12f8d1a253a
KarenReyna <karen.reyna95@gmail.com>
2016-03-18 22:24:41 -0600

    Fondo degradado

---
+++ KimiTrivia/ControllerNiveles.swift
@@ -0,0 +1,9 @@
+//
+//  ControllerNiveles.swift
+//  KimiTrivia
+//
+//  Created by lizbeth garza walle on 18/03/16.
+//  Copyright © 2016 Ana Karen Reyna. All rights reserved.
+//
+
+import Foundation
--- KimiTrivia/ViewController.swift
+++ KimiTrivia/ViewController.swift
@@ -10,10 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UIVLogo: UIImageView!
+    
+    var logo1: UIImage = UIImage(named: "Logo1")!
+    var logo2: UIImage = UIImage(named: "Logo2")!
+    var logo3: UIImage = UIImage(named: "Logo3")!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UIVLogo.animationImages = [logo1, logo2, logo3]
+        UIVLogo.animationDuration = 0.75
+        UIVLogo.startAnimating()
+        
+        
     }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
